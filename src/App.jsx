f14d88919ff3d752bed4a1742ca4a160984f06c0
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-10-02 11:35:41 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import LandingPage from "./pages/LandingPage";
+import Dashboard from "./pages/Home/Dashboard";
+import EditResume from "./pages/ResumeUpdate.jsx/EditResume";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <Router>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/resume/:resumeId" element={<EditResume />} />
+          </Routes>
+        </Router>
+      </div>
+
+      <Toaster
+        toastOptions={{
+          className: "",
+          style: {
+            fontSize: "13px",
+          },
+        }}
+      />
+    </>
+  );
+};
+
+export default App;
