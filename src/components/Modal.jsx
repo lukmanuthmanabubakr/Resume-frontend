c5906dd43dc30571e7b55fb8db5408badeb9bc67
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-10-04 21:35:32 +0100

    update

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -25,6 +25,22 @@ const Modal = ({
             )}
           </div>
         )}
+
+        <button type="button" className="" onClick={onclose}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className=""
+            fill="none"
+            viewBox="0 0 14 14"
+            strokeWidth={1.8}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
       </div>
     </div>
   );
