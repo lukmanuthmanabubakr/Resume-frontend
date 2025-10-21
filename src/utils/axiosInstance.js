d433ea6d57209916f60a4b30b056afb399ca033c
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-10-21 03:18:04 +0100

    update

---
+++ src/utils/axiosInstance.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { BASE_URL } from "./apiPath";
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+//Request Interceptor
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("token");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+//Response Interceptor
