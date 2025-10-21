2f16186a341bf97a334523889ae294cf032029ab
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-10-21 03:32:13 +0100

    okay done

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -25,3 +25,24 @@ axiosInstance.interceptors.request.use(
 );
 
 //Response Interceptor
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    //Handle common errors globally
+    if (error.response) {
+      if (error.response.status === 401) {
+        //Redirect to login page
+        window.location.href = "/";
+      } else if (error.respose.status === 500) {
+        console.error("Server error. Please try again later.");
+      }
+    } else if (error.code === "ECONNABORTED") {
+      console.error("Request timeout, Please try again");
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
