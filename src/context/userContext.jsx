cc4e32d91318712ee4a940668ff679a9478a4742
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-10-21 17:14:38 +0100

    update

---
+++ src/context/userContext.jsx
@@ -0,0 +1,43 @@
+import React, {
+  CreateContext,
+  useState,
+  useEffect,
+  createContext,
+} from "react";
+import axiosInstance from "../utils/axiosInstance.js";
+import { API_PATHS } from "../utils/apiPath.js";
+
+export const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const { user, setUser } = useState(null);
+  const { loading, setLoading } = useState(true); //New State to track loading
+
+  useEffect(() => {
+    if (user) return;
+
+    const accessToken = localStorage.getItem("token");
+    if (!accessToken) {
+      setLoading(false);
+      return;
+    }
+    const fetchUser = async () => {
+      try {
+        const response = await axiosInstance.get(API_PATHS.AUTH.GET_PROFILE);
+        setUser(response.data);
+      } catch (error) {
+        console.error("User not authorized", error);
+        clearUser();
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchUser();
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ user, loading, updateUser, clearUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
