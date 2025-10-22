af9f52bfe9a4dcd327349d8eacc57741b41e4e39
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-10-22 01:22:01 +0100

    update

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 import Input from "../../components/inputs/Input";
 import { validateEmail } from "../../utils/helper";
 import { UserContext } from "../../context/userContext";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPath";
 
 const Login = ({ setCurrentPage }) => {
   const [email, setEmail] = useState("");
@@ -27,8 +29,24 @@ const Login = ({ setCurrentPage }) => {
 
     //Login Api call
     try {
-      
-    } catch (error) {}
+      const response = await axiosInstance.post(API_PATHS.AUTH.LOGIN, {
+        email,
+        password,
+      });
+
+      const { token } = response.data;
+      if (token) {
+        localStorage.setItem("token", token);
+        updateUser(response.data);
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError("Something went wrong. Please try again");
+      }
+    }
   };
 
   return (
