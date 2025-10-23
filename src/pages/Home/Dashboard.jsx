14404bcedb0c0e7ac7c7da8ce646f03aae673752
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-10-23 03:07:03 +0100

    update

--- src/pages/Home/Dashboard.jsx
+++ src/pages/Home/Dashboard.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPath";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Dashboard
+  const [openCreateModal, setOpenCreateModal] = useState(false);
+  const [allResumes, setAllResumes] = useState(null);
+
+  const fetchAllResumes = async () => {
+    try {
+      const response = await axiosInstance.get(API_PATHS.RESUME.GET_ALL);
+      setAllResumes(response.data);
+    } catch (error) {
+      console.error("Error fetching resumes", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllResumes();
+  }, []);
+  return <DashboardLayout>Dashboard</DashboardLayout>;
+};
+
+export default Dashboard;
