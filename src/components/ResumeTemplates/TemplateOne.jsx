b51605dea0f0a2ffbc9b120bcd7754b82088aef2
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-10-30 17:47:11 +0100

    update

--- src/components/ResumeTemplates/TemplateOne.jsx
+++ src/components/ResumeTemplates/TemplateOne.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 
-const TemplateOne = ({ resumeData, 
-    colorPalette,
-     containerwidth }) => {
+const TemplateOne = ({ resumeData, colorPalette, containerwidth }) => {
   return <div>TemplateOne</div>;
 };
 
