0beffcac6a0d2cca5d4390b8177b5a3e9f80c238
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-10-31 17:28:27 +0100

    Haloween

--- src/components/ResumeTemplates/TemplateOne.jsx
+++ src/components/ResumeTemplates/TemplateOne.jsx
@@ -1,4 +1,12 @@
 import React from "react";
+import {
+  LuMail,
+  LuPhone,
+  LuMapPinHouse,
+  LuRss,
+  LuGithub,
+  LuUser,
+} from "react-icons/lu";
 
 const TemplateOne = ({ resumeData, colorPalette, containerwidth }) => {
   return <div>TemplateOne</div>;
