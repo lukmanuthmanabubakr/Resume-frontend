0834af522c5d528684156cc1fb1e0af33aee6b36
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-10-31 17:50:36 +0100

    update

--- src/components/ResumeTemplates/TemplateOne.jsx
+++ src/components/ResumeTemplates/TemplateOne.jsx
@@ -7,9 +7,23 @@ import {
   LuGithub,
   LuUser,
 } from "react-icons/lu";
+import { RiLinkedinLine } from "react-icons/ri";
+
+const DEFAULT_THEME = ["#EBFDFF", "#A1F4FD", "#CEFAFE", "#008ED5", "#AA5565"];
+
+const Title = ({ text, color }) => {
+  return (
+    <div className="relative w-fit mb-2.5">
+      <span className="absolute bottom-0 left-0 w-full h-2" style={{ backgroundColor: color }}></span>
+      <h2 className={"relative text-sm font-bold"}>{text}</h2>
+    </div>
+  );
+};
 
 const TemplateOne = ({ resumeData, colorPalette, containerwidth }) => {
-  return <div>TemplateOne</div>;
+  return <div>
+    
+  </div>;
 };
 
 export default TemplateOne;
