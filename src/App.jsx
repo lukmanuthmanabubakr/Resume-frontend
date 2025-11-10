69cf64ba2513f050026b1b20ba45edf72e25e2cd
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-11-10 20:58:41 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,9 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
-import LandingPage from "./pages/LandingPage";
-import Dashboard from "./pages/Home/Dashboard";
-import EditResume from "./pages/ResumeUpdate.jsx/EditResume";
-import UserProvider from "./context/userContext";
+import React from 'react'
 
 const App = () => {
   return (
-    <UserProvider>
-      <div>
-        <Router>
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/resume/:resumeId" element={<EditResume />} />
-          </Routes>
-        </Router>
-      </div>
+    <div>App</div>
+  )
+}
 
-      <Toaster
-        toastOptions={{
-          className: "",
-          style: {
-            fontSize: "13px",
-          },
-        }}
-      />
-    </UserProvider>
-  );
-};
-
-export default App;
+export default App
