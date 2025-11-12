caeb3379e56a0d49d8ebe88d2afc8edb9ec35852
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-11-12 23:13:14 +0100

    updte

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { dummyResumeData } from "../assets/assets";
+import { XIcon } from "lucide-react";
 
 const Dashboard = () => {
   const colors = ["#9333ea", "#d97706", "#dc2626", "#0284c7", "#16a34a"];
   const [allResumes, setAllResumes] = useState([]);
   const [showCreateResume, setShowCreateResume] = useState(false);
   const [showUploadResume, setShowUploadResume] = useState(false);
+  const [title, setTitle] = useState("");
+  const [resume, setResume] = useState(null);
+  const [editResumeId, setEditResumeId] = useState("");
 
   const loadAllResumes = async () => {
     setAllResumes(dummyResumeData);
@@ -205,10 +209,29 @@ const Dashboard = () => {
         </div>
 
         {showCreateResume && (
-          <form className="fixed inset-0 bg-black/70 backdrop-blur bg-opacity-50 z-10 flex items-center justify-center">
-            <div>
-              <h2>Create Resume</h2>
-              <input type="text" placeholder="Enter resume title"/>
+          <form onSubmit={creatRueme} className="fixed inset-0 bg-black/70 backdrop-blur bg-opacity-50 z-10 flex items-center justify-center">
+            <div
+              onClick={(e) => e.stopPropagation()}
+              className="relative bg-slate-50 border shadow-md rounded-lg
+              w-full max-w-sm p-6"
+            >
+              <h2 className="text-xl font-bold mb-4">Create Resume</h2>
+              <input
+                type="text"
+                placeholder="Enter resume title"
+                className="w-full px-4 py-2 mb-4 focus:border-green-600 ring-green-600"
+                required
+              />
+              <button className="w-full py-2 bg-green-600 text-white rounded hover:bg-gray-700 transition-colors">
+                Create Resume
+              </button>
+              <XIcon
+                className="absolute top-4 right-4 text-slate-400 hover:text-slate-600 cursor-pointer transition-colors"
+                onClick={() => {
+                  setShowCreateResume(false);
+                  setTitle("");
+                }}
+              />
             </div>
           </form>
         )}
