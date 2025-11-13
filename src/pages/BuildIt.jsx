fd4c41a0629875e2c095990d424c4aef0e539f2c
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-11-13 14:36:52 +0100

    update

--- src/pages/BuildIt.jsx
+++ src/pages/BuildIt.jsx
@@ -12,6 +12,7 @@ import {
   Sparkles,
   User,
 } from "lucide-react";
+import PersonalInfoForm from "../components/PersonalInfoForm";
 
 const BuildIt = () => {
   const { resumeId } = useParams();
@@ -118,9 +119,18 @@ const BuildIt = () => {
 
               {/* Form Section */}
               <div className="space-y-6">
-                  {activeSection.id === 'personal' && (
-                    <div></div>
-                  )}
+                {activeSection.id === "personal" && (
+                  <PersonalInfoForm
+                    data={resumeData.personal_info}
+                    onChange={(data) =>
+                      setResumeData((prev) => ({
+                        ...prev,
+                        personal_info: data,
+                      }))
+                    }
+                    removeBackground={removeBackground} setRemoveBackground={setRemoveBackground}
+                  />
+                )}
               </div>
             </div>
           </div>
