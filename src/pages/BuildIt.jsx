c411bf414c9129f6234e7c1ee3c3e10346aae9d0
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-11-14 22:16:16 +0100

    update

--- src/pages/BuildIt.jsx
+++ src/pages/BuildIt.jsx
@@ -15,6 +15,7 @@ import {
 import PersonalInfoForm from "../components/PersonalInfoForm";
 import ResumePreview from "../components/ResumePreview";
 import TemplateSelector from "../components/TemplateSelector";
+import ColorPicker from "../components/ColorPicker";
 
 const BuildIt = () => {
   const { resumeId } = useParams();
@@ -87,13 +88,14 @@ const BuildIt = () => {
 
               {/* Navigation section */}
               <div className="flex justify-between items-center mb-6 border-b border-gray-300 py-1">
-                <div className="flex justify-between items-center mb-6 border-b border-gray-300 py-1">
+                <div className="flex items-center gap-2">
                   <TemplateSelector
                     selectedTemplate={resumeData.template}
                     onChange={(template) =>
                       setResumeData((prev) => ({ ...prev, template }))
                     }
                   />
+                  <ColorPicker />
                 </div>
                 <div className="flex items-center">
                   {activeSectionIndex !== 0 && (
