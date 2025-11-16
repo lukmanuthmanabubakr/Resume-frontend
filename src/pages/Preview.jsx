1cff2c1d329847b40cc264b6cda48891926e4b48
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-11-16 09:55:06 +0100

    upda

--- src/pages/Preview.jsx
+++ src/pages/Preview.jsx
@@ -1,12 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { dummyResumeData } from "../assets/assets";
 
 const Preview = () => {
   const { resumeId } = useParams;
   const [resumeData, setResumeData] = useState(null);
   const loadResume = async () => {
-    setResumeData()
+    setResumeData(dummyResumeData.find(resume => resume._id === resumeId || null))
   }
+
+  useEffect(() => {
+    loadResume()
+  }, [])
   return <div>Preview Page</div>;
 };
 
