ab6fefaebf3227d2a81e1e96b1300a7b942f86f8
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-11-16 09:42:24 +0100

    update

--- src/pages/Preview.jsx
+++ src/pages/Preview.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Preview = () => {
   const { resumeId } = useParams;
-  const
+  const [resumeData, setResumeData] = useState(null);
   return <div>Preview Page</div>;
 };
 
