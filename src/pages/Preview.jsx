a5623369500b6e3e77bb35cdac8fc6cf434cd854
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-11-16 09:41:03 +0100

    update

--- src/pages/Preview.jsx
+++ src/pages/Preview.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
 
 const Preview = () => {
-  const {resumeId}
-  return (
-    <div>Preview Page</div>
-  )
-}
+  const { resumeId } = useParams;
+  return <div>Preview Page</div>;
+};
 
-export default Preview
+export default Preview;
