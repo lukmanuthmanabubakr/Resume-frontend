d9345e509e4da81b6d53e59114dd1a4ef7fcc11a
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-11-21 09:35:11 +0100

    update

--- src/components/ExperienceForm.jsx
+++ src/components/ExperienceForm.jsx
@@ -1,6 +1,8 @@
 import { Briefcase, Plus, Sparkles, Trash2 } from "lucide-react";
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import api from "../configs/api";
+import toast from "react-hot-toast";
 
 const ExperienceForm = ({ data, onChange }) => {
   const { token } = useSelector((state) => state.auth);
@@ -28,10 +30,22 @@ const ExperienceForm = ({ data, onChange }) => {
   };
 
   const generateDescription = async (index) => {
-    setGeneratingIndex(index)
-    const experience = data[index]
-    
-  }
+    setGeneratingIndex(index);
+    const experience = data[index];
+    const prompt = `enhance this job description ${experience.description} for the position of ${experience.position} at ${experience.company}.`;
+    try {
+      const { data } = await api.post(
+        "/api/ai/enhance-job-desc",
+        { userContent: prompt },
+        { headers: { Authorization: token } }
+      );
+      updateExperience(index, "description", data.enhancedContent);
+    } catch (error) {
+      toast(error.message);
+    } finally {
+      setGeneratingIndex(-1);
+    }
+  };
 
   return (
     <div className="space-y-6">
