1636fc9ea92fc5ff673f0a23b0eae4f7f895db5c
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-11-21 10:23:53 +0100

    Update

--- src/pages/Preview.jsx
+++ src/pages/Preview.jsx
@@ -4,16 +4,21 @@ import { dummyResumeData } from "../assets/assets";
 import ResumePreview from "../components/ResumePreview";
 import Loader from "../components/Loader";
 import { ArrowLeftIcon } from "lucide-react";
+import api from "../configs/api";
 
 const Preview = () => {
-  const { resumeId } = useParams(); 
+  const { resumeId } = useParams();
   const [isLoading, setIsLoading] = useState(true);
   const [resumeData, setResumeData] = useState(null);
   const loadResume = async () => {
-    setResumeData(
-      dummyResumeData.find(resume => resume._id === resumeId || null)
-    );
-    setIsLoading(false);
+    try {
+      const { data } = await api.get("/api/resumes/public/" + resumeId);
+      setResumeData(data.resume);
+    } catch (error) {
+      console.error(error.message);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   useEffect(() => {
@@ -39,7 +44,10 @@ const Preview = () => {
           <p className="text-center text-6xl text-slate-400 font-medium">
             Resume not found
           </p>
-          <Link to="/" className="mt-6 bg-green-500 hover:bg-green-600 text-white rounded-full px-6 h-9 m-1 ring-offset-1 ring-1 ring-green-400 flex items-center transition-colors">
+          <Link
+            to="/"
+            className="mt-6 bg-green-500 hover:bg-green-600 text-white rounded-full px-6 h-9 m-1 ring-offset-1 ring-1 ring-green-400 flex items-center transition-colors"
+          >
             <ArrowLeftIcon className="mr-2 size-4" />
             Go to home page
           </Link>
