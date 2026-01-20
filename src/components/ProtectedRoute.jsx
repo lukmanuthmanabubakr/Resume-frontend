2463024b6e78610fded1ea542f3e1737ac53b369
dev_dimeji✔ <lukmanuthman135@gmail.com>
2026-01-20 20:32:58 +0100

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,4 +1,4 @@
-// src/components/ProtectedRoute.jsx
+// src//ProtectedRoute.jsx
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
